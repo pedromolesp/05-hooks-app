ac1983a48e4a3eedf99acca3c645d8d755b9d820
Pedro Molina <pedro.molina@petroprix.com>
2021-06-08 23:51:02 +0200

    añadido useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/01-useState/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
 // import { SimpleForm } from './components/01-useState/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-  <RealExampleRef />,
+  <Layout />,
   document.getElementById('root')
 );
 
